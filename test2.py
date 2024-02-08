6197206c8279f8b830ad808d3b2c56452c528ba8
KTC-SatoshiAYAKAWA <satoshi.ayakawa@kinto-technologies.com>
2024-02-08 11:26:20 +0900

    test2

---
+++ test2.py
@@ -0,0 +1,13 @@
+import torch
+import timeit
+gpu_device = "mps"
+dtype = torch.float
+number = 30
+size = 8192
+cpumat = torch.randn(size,size,dtype=dtype)
+gpumat = cpumat.to(gpu_device)
+
+cpu_time = timeit.timeit("torch.matmul(cpumat,cpumat)",number=number,globals=globals())
+print("cpu_time: {:.6f} seconds".format(cpu_time))
+gpu_time = timeit.timeit("torch.matmul(gpumat,gpumat)",number=number,globals=globals())
+print("gpu_time: {: .6f} seconds".format(gpu_time))
