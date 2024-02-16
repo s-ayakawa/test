244d7020fd1db932474c19c3cdd0d537f9fc72fb
KTC-SatoshiAYAKAWA <satoshi.ayakawa@kinto-technologies.com>
2024-02-16 09:40:16 +0900

    commit

--- test11.sql
+++ test11.sql
@@ -3,9 +3,11 @@ select
   t1.default_channel_group_ga4_nondirect as "default_channel_group_ga4_nondirect",
   count(tealium_unique_session_id)
 from
-  prod_kintojpn_view.view_wlog_tealium_viewevents_basic as t1 /* TealiumWebログ(本番ページビューイベントのみ) */
+  prod_kintojpn_view.view_wlog_tealium_allevents_basic as t1 /* TealiumWebログ(本番全ログ) */
 where
-  t1.event_date_local = Date '2024-01-13'
+  t1.event_date_local >= date'2024-02-01'
+  and t1.pageurl_domain = 'kinto-jp.com'
+  and t1.udo_tealium_event = 'view'
 group by
   t1.event_date_local,
   t1.default_channel_group_ga4_nondirect
